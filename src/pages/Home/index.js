43203a36d073e93a920a06f3a584e7002f36a143
João Paulo Mazzo <mazzojp@gmail.com>
2022-06-21 07:47:54 -0300

    feat: add tailwindcss in project

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-import * as S from './styled';
+import githubLogo from './../../assets/github_logo.jpg';
 
 export default function Home(props) {
   const navigate = useNavigate();
@@ -22,13 +22,27 @@ export default function Home(props) {
       })
   }
 
+  const handleUsuarioChange = (event) => setUsario(event.target.value);
+
   return (
-    <S.HomeContainer>
-      <S.Content>
-        <S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsario(e.target.value)} />
-        {error ? <S.ErrorMsg>Ocorreu um erro. Tente novamente</S.ErrorMsg> : ''}
-        <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
-      </S.Content>
-    </S.HomeContainer>
+    <main className='grid grid-cols-2 h-screen text-white'>
+      <div className='flex items-center justify-center bg-purple'>
+        <div className='flex items-center justify-center flex-row flex-wrap w-[450px] bg-gray-black rounded-2xl'>
+          <h1 className='text-4xl font-bold mt-20 mb-16'>GitHub user searcher</h1>
+          <form className='flex flex-row flex-wrap w-80'>
+            <label>
+              <span className='text-xs ml-3'>Username:</span>
+              <input className='w-80 h-10 rounded-2xl' type="text" name="username" value={usuario} onChange={handleUsuarioChange}></input>
+            </label>
+            <button type="submit"></button>
+          </form>
+        </div>
+      </div>
+      <div className='flex items-center justify-center bg-gray-black'>
+        <img src={githubLogo} alt="logo " />
+      </div>
+    </main >
   );
 }
+  //       { error ? <S.ErrorMsg>Ocorreu um erro. Tente novamente</S.ErrorMsg> : '' }
+  // <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray-black': '#212227',
+        'purple': '#5D54A4',
+      }
+    },
+  },
+  plugins: [],
+}
