1c13e8792ee5dbb788278745ada4e99df92f980b
João Paulo Mazzo <mazzojp@gmail.com>
2022-07-03 00:09:04 -0300

    update: implement ApolloProvider and the client to the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
+import { ApolloProvider } from '@apollo/client';
+import client from './plugins/apollo/client';
+
+import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
